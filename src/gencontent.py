5d8b155afe4152a1940bb78acd65bf96b75db1cc
palleja03 <palleja03@gmail.com>
2024-07-22 00:20:34 +0200

    add page generation recursively

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,12 +1,16 @@
-from markdown_blocks import markdown_to_blocks, markdown_to_html_node
+from markdown_blocks import markdown_to_html_node
+from pathlib import Path
 import os
 
-def extract_title(md):
-    lines = md.split("\n")
-    for line in lines:
-        if line.startswith("# "):
-            return line[2:]
-    raise ValueError("No title found")
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
@@ -27,6 +31,9 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, "w") as f:
         f.write(new_html)
 
-if __name__ == "__main__":
-    print(os.path.exists("./el_nabo/peludo/index.html"))
-    print(generate_page("./content/index.md", "./template.html", "./el_nabo/peludo/index.html"))
+def extract_title(md):
+    lines = md.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
+    raise ValueError("No title found")
